d767a64dec37405d266aacde565b84b56d17cade
anderzzz <ohrn.anders@gmail.com>
2016-12-10 12:07:07 -0800

    Modification to main based on webapi class

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import sys
-import urllib2
+
+from webapis import PDBData
 
 def main(args):
 
-    print args
+    print (args)
+    pdb_data = PDBData()
+    print (pdb_data.url_base)
+    pdb_data.set_format_return('csv4')
 
-    pass
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
