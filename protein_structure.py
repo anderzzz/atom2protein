2daa63b6450aa12aca41a409b44614ff6741b18f
anderzzz <ohrn.anders@gmail.com>
2016-12-14 14:01:37 -0800

    Added protein structure class

---
+++ protein_structure.py
@@ -0,0 +1,18 @@
+'''Bla bla
+
+'''
+class ProteinStructure:
+    '''Bla bla
+
+    '''
+    def _populate_from_pdb(self, pdb_string):
+        '''Bla bla
+
+        '''
+        pass
+
+    def __init__(self, pdb_string):
+        '''Bla bla
+
+        '''
+        self._populate_from_pdb(pdb_string)
