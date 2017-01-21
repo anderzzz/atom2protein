6e392a4385af9c3c0e742cf23cf4166060fb3fb5
anderzzz <ohrn.anders@gmail.com>
2017-01-20 16:09:56 -0800

    Minor styling of form with clearer labels added

--- server/presenter_webapp/forms.py
+++ server/presenter_webapp/forms.py
@@ -32,7 +32,7 @@ class SearchStructureForm(forms.ModelForm):
                   'depositdate_min' : _('Earliest deposit date'),
                   'depositdate_max' : _('Latest deposit date'),
                   'molweight_min' : _('Minimum molecular weight'),
-                  'molweight_max' : _('maximum molecular weight')}
+                  'molweight_max' : _('Maximum molecular weight')}
         help_texts = {'depositdate_min' : _('Format YYYY-MM-DD'),
                       'depositdate_max' : _('Format YYYY-MM-DD'),
                       'description' : _('Comma-separated terms'),
@@ -53,9 +53,25 @@ class SummaryPropertyForm(forms.ModelForm):
         '''
         model = RetrieverStructure
         fields = ('nresidues', 'rresidues_polarity', 'bb_torsions')
+        labels = {'nresidues' : _('Number of residues'),
+                  'rresidues_polarity' : _('Percentage residues of polarity class'),
+                  'bb_torsions' : _('Backbone torsion angles')}
+        help_texts = {'rresidues_polarity' : _('Residues are of polarity class')}
 
 class PresentationVizForm(forms.ModelForm):
+    '''Class that defines the form for how to present summarization.
+
+    Notes
+    -----
+    The items of the form are taken from a Django model, but only the items
+    related to the presentation are kept in the ``Meta`` subclass.
+
+    '''
  
     class Meta:
+        '''Define fields of the form. A subset from a Django model
+
+        '''
         model = RetrieverStructure
         fields = ('collective_viz', )
+        labels = {'collective_viz' : _('Collective vizualization of summaries')}
